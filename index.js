cbffc73d68ab673a0b67d959aae74f0b98919f2b
Nilotpal Das <neelot3pal@gmail.com>
2020-10-11 01:44:45 +0530

    n

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express=require('express');
 
 const port=8000;
 const app=express();
-const db = require('./config/mongoose');
-const contact =require('./models/contact');
+
 //use express router
 app.use('/',require('./routes'));
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views', './views');
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('assets'));
 app.listen(port,function(err){
     if(err)
